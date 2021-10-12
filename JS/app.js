84a694d20272211f36284520a2bf9aa9cc7d0211
1w1111957TodescoNatalia <nstodesco@gmail.com>
2021-10-12 12:43:59 -0300

    Inicio
    Pagina de inicio con login

---
+++ JS/app.js
@@ -0,0 +1,41 @@
+  function mostrarMensaje() {
+    swal("Información del Sistema","Trabajo práctico integrador realizado para la materia Metodología de Sistemas I, de la Tecnicatura Universitaria en Programación, ciclo lectivo 2021. Que tiene como objetivo  llevar adelante un proyecto de desarrollo de software a medida, aplicando los conceptos teóricos y prácticos aprendidos durante el desarrollo de la asignatura");
+  }
+
+  function go(){
+    // Validación del Form
+    var forms = document.getElementsByClassName('needs-validation');
+      
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('click', function (event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+
+    // Login
+    var rol = document.getElementById('rol');
+    var idRol = rol.selectedIndex+1;
+
+    if ($("#contraseña").val()=='123456' && $("#email").val() =='administrador@gmail.com' && idRol == 1){ 
+      window.location.replace("./menu.html");
+    } 
+    
+    else if ($("#contraseña").val()=='123456' && $("#email").val() =='gerente@gmail.com' && idRol == 2){
+      window.location.replace("./menu.html");
+    } 
+    
+    else if ($("#contraseña").val()=='123456' && $("#email").val() =='transportista@gmail.com' && idRol == 4){
+      window.location.replace("");
+    } 
+    
+    else if ($("#contraseña").val()=='123456' && $("#email").val() =='empleado@gmail.com' && idRol == 3){
+      window.location.replace("");
+    }
+    else{ 
+      swal("Error de Validacion","Porfavor ingrese email, rol y contraseña correctos","error"); 
+    } 
+    } 
