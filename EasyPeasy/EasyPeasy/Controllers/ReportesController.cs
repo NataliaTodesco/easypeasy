e91a50185b4e77efc8caccaa3bead650330af861
Deni22e <griseldadenisfernandez@gmail.com>
2021-11-08 11:40:07 -0300

    update reportes

--- EasyPeasy/EasyPeasy/Controllers/ReportesController.cs
+++ EasyPeasy/EasyPeasy/Controllers/ReportesController.cs
@@ -19,14 +19,14 @@ namespace EasyPeasy.Controllers
         [Route("/Reportes/EntregasPorTransportista")]
         public ActionResult<ResultadoApi> EntregasPorTransportista()
         {
-            Random random = new Random();
+            //Random random = new Random();
             //listado de transportistas
             var transportistasList = _db.Transportistas.ToList();
             var entregasList = new List<ReportesViewModel>();
             ResultadoApi resultadoApi = new ResultadoApi();
             try
             {
-                if (transportistasList != null)
+               if (transportistasList != null)
                 {
                     int total = 100;
                     for (int i = 0; i < transportistasList.Count(); i++)
@@ -34,7 +34,8 @@ namespace EasyPeasy.Controllers
                         ReportesViewModel report = new ReportesViewModel()
                         {
                             text = transportistasList[i].Nombre,
-                            value = random.Next(total),
+                            // value = random.Next(total),
+                            value=_db.HojaRuta.Where(x=>x.IdTransportista==transportistasList[i].IdTransportista).Count(),
                             color = String.Format("#{0:X6}", new Random().Next(0x1000000))
                         };
                         total -= report.value;
@@ -58,16 +59,17 @@ namespace EasyPeasy.Controllers
         [Route("/Reportes/EstadoEntregas")]
         public ActionResult<ResultadoApi> EstadoEntregas()
         {
-            Random random = new Random();
+            //Random random = new Random();
             ResultadoApi resultadoApi = new ResultadoApi();
             try
             {
-                int total = 100;
-                var entregadas = random.Next(total);
-                var reprogramadas = random.Next(total - entregadas);
-                int[] estados = new int[2];
-                estados[0] = entregadas;
-                estados[1] = reprogramadas;
+                // int total = 100;
+                // var entregadas = random.Next(total);
+                // var reprogramadas = random.Next(total - entregadas);
+                // int[] estados = new int[2];
+                var estados= new List<int>();
+                estados.Add(_db.Remitos.Where(x=>x.IdEstado==3).Count());
+                estados.Add(_db.Remitos.Where(x=>x.IdEstado==4).Count());
 
                 resultadoApi.Ok = true;
                 resultadoApi.Return = estados;
